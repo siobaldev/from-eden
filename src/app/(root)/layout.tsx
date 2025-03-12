c279c8ab3aa66cdbda603d20fe0fc33e4df80169
br0wnrecluse <sblmnrd@gmail.com>
2025-03-12 15:45:19 +0800

    feat(header): implement responsive header with navigation
    - Add responsive header layout
    - Install lucide-react package for UI icons
    - Add fluid typography configuration to globals.css
    - Create siteConfig for storing static data

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,7 +1,13 @@
+import Header from "@/components/header";
 export default function layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <main>{children}</main>;
+  return (
+    <main>
+      <Header />
+      {children}
+    </main>
+  );
 }
---
+++ src/app/siteConfig.tsx
@@ -0,0 +1,26 @@
+export const siteConfig = {
+  sections: {
+    tableOfContents: [
+      {
+        id: "home",
+        label: "Home",
+      },
+      {
+        id: "Shop",
+        label: "Shop",
+      },
+      {
+        id: "ocassions",
+        label: "Ocassions",
+      },
+      {
+        id: "about-us",
+        label: "About Us",
+      },
+      {
+        id: "contact",
+        label: "Contact",
+      },
+    ],
+  },
+};
