62ae1a1cc9773615c8a0065392277d31a6916cc3
br0wnrecluse <sblmnrd@gmail.com>
2025-03-14 22:57:00 +0800

    feat(styles): refactor CSS layers and add new wrapper class
    - Move body class to @layer base
    - Create wrapper class inside @layer components
    - Add font sizes to @theme in globals.css
    - Apply wrapper class to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,37 +5,35 @@ import { siteConfig } from "@/app/siteConfig";
 
 export default function Header() {
   return (
-    <>
-      <header className="container px-4 mx-auto py-4 lg:py-6 w-full items-center flex justify-between">
-        <div className="lg:hidden">
-          <AlignJustify />
-        </div>
+    <header className="wrapper py-4 lg:py-6 items-center flex justify-between">
+      <div className="lg:hidden">
+        <AlignJustify />
+      </div>
 
-        <Link href={"/"} className="flex items-center">
-          <Image
-            className="w-16 h-auto lg:w-20"
-            src={"/assets/from-eden-logo.svg"}
-            alt="from-eden-logo"
-            width={80}
-            height={80}
-          />
-        </Link>
+      <Link href={"/"} className="flex items-center">
+        <Image
+          className="w-16 h-auto lg:w-20"
+          src={"/assets/from-eden-logo.svg"}
+          alt="from-eden-logo"
+          width={80}
+          height={80}
+        />
+      </Link>
 
-        <nav className="hidden lg:block">
-          <ul className="flex font-bold gap-x-10">
-            {siteConfig.sections.tableOfContents.map((section, index) => (
-              <li key={index}>
-                <Link href={`#${section.id}`}>{section.label}</Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-        <div className="flex gap-x-3.5">
-          <Search className="hidden lg:block" />
-          <ShoppingBag />
-          <UserRound className="hidden lg:block" />
-        </div>
-      </header>
-    </>
+      <nav className="hidden lg:block">
+        <ul className="flex font-bold gap-x-10">
+          {siteConfig.sections.tableOfContents.map((section, index) => (
+            <li key={index}>
+              <Link href={`#${section.id}`}>{section.label}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <div className="flex gap-x-3.5">
+        <Search className="hidden lg:block" />
+        <ShoppingBag />
+        <UserRound className="hidden lg:block" />
+      </div>
+    </header>
   );
 }
